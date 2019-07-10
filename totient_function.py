1298da140591af0d0d18e03b46ac0a9c1216d547
Jack <crellin_jack@yahoo.com.au>
2019-07-10 20:34:05 +1000

    eulers phi function

--- totient_function.py
+++ totient_function.py
@@ -1,7 +1,9 @@
 import prime_counter
+import itertools
+import numpy as np
 
-# brute force approach
-def get_relative_primes(n, largest):
+# basic and inefficient approach
+def get_num_coprimes(n, largest):
 	totients = [1]
 	i = 2
 	while i < n: 
@@ -9,13 +11,34 @@ def get_relative_primes(n, largest):
 			totients.append(i)
 		i += 1
 
-	return totients
+	return len(totients)
 
 def gcd(a, b):
     if b ==0:
        return a 
     else:
        return gcd(b, a % b);
+
+# more efficient brute-force approach
+def prime_product_method(n, primes = [2,3,5,7,11,13,17,19]):
+	prod = 1
+	if n >= 2:
+		idx = 0
+		products = set()
+		n_cpy = n
+		diff = 0
+		while idx < len(primes) and n_cpy != 1:
+			if n_cpy % primes[idx] == 0:
+				products.add(primes[idx])
+				n_cpy /= primes[idx]
+			else:
+				idx += 1
+
+		prod = n
+		for p in products:
+			prod *= (1-1/p)
+
+	return int(prod)
 
 # proposition: 	for any bound n, the number with the greatest ratio of x to its number of coprimes
 #				will be the product of the most, smallest, consecutive primes possible for x to be 
@@ -25,26 +48,21 @@ def gcd(a, b):
 #					n = 100: 	x = 30 = 2*3*5 
 #					n = 1000:	x = 210 = 2*3*5*7
 
-def prime_product_method(n, primes):
+def maximum_primes_solution(upper):
+	result = 1
 	idx = 0
-	products = set()
-	n_cpy = n
-	while idx < len(primes) and n_cpy != 1:
-		if n_cpy % primes[idx] == 0:
-			products.add(primes[idx])
-			n_cpy /= primes[idx]
-		else:
-			idx += 1
+	root = np.floor((np.sqrt(upper))).astype('int32')
+	primes = prime_counter.prime_counter(root)
 
-	totients = 210
+	while result < upper and idx < len(primes):
+		result *= primes[idx]
+		idx += 1
+
+	return result / primes[idx-1]
+
 
 if __name__ == "__main__":
-	p = [2,3,5,7,11]
-	prime_product_method(210,p)
-	combs = set()
-	l = ['a','b','c','d']
-	for no in range(1, len(l)+1):
-		for i in range(0,len(l)):
-			combs.add("".join(l[i:i+no]))
 
-	print(combs)
+	print(maximum_primes_solution((1000000)))
+	# 510510
+	
